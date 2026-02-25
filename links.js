abe900b61a79f6eec4549c3400302de4917ca2e4
Romesa3970 <115862901+Romesa3970@users.noreply.github.com>
2026-02-25 10:44:25 +0500

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
-const affiliateLink = "https://onelink.shein.com/30/5hgzbo05ewbi";
-const affiliateLink = "https://onelink.shein.com/30/5hh0lxa8fswz";
+const affiliateLink = "https://artistry-academy.com/lp/#aff=Rosehook";
+
 
 
 
